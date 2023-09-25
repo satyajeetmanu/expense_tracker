38d310f652689d167118e58f815e0c55e50fb3a2
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2023-09-25 12:57:49 +0530

    implemented ListView to display dummy expense data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -8,13 +10,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter course',
+        amount: 100.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Movie',
+        amount: 250.19,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('Chart'),
-          Text('Expense list'),
+          const Text('Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
         ],
       ),
     );
